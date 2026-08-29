eb010d3f1572f0f3963069763ccd1adb67fd64ac
Abdelrhman Essam <234604709+abdelrhmanmaroxo-spec@users.noreply.github.com>
2026-08-29 19:48:46 +0300

    bump app shell cache after search and language fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pathpilot-v4';
+const CACHE_NAME = 'pathpilot-v5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
